bc66f9bb0d830c50508fea43fc1fb5a33fc9c8ec
elyvanbs <elyvanbs@gmail.com>
2025-08-25 19:44:25 -0500

    feat: Cria Branch

--- sgcmapi/src/main/java/br/ufac/sgcmapi/model/EStatus.java
+++ sgcmapi/src/main/java/br/ufac/sgcmapi/model/EStatus.java
@@ -2,20 +2,20 @@ package br.ufac.sgcmapi.model;
 
 public enum EStatus {
 
-    CANCELADO,
-    AGENDADO,
-    CONFIRMADO,
-    CHEGADA,
-    ATENDIMENTO,
-    ENCERRADO;
+  CANCELADO,
+  AGENDADO,
+  CONFIRMADO,
+  CHEGADA,
+  ATENDIMENTO,
+  ENCERRADO;
 
-    public EStatus proximo() {
-        var status = this;
-        var index = ordinal() + 1;
-        if (index > 1 && index < values().length) {
-            status = values()[index];
-        }
-        return status;
+  public EStatus proximo() {
+    var status = this;
+    var index = ordinal() + 1;
+    if (index > 1 && index < values().length) {
+      status = values()[index];
     }
+    return status;
+  }
 
 }
